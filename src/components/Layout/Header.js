c02d7600e19b9b23b2fbb6cd63cc5e17342ae20a
Aleksa Piperski <mr.alexpiperski@gmail.com>
2025-06-13 16:25:44 +0200

    Header base elements configured

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,3 +1,17 @@
-const Header = (props) => {};
+import mealsImage from "../../assets/meals.jpg";
+import classes from "./Header.module.css";
+const Header = (props) => {
+  return (
+    <>
+      <header className={classes.header}>
+        <h1 className={classes.h1}>ReactMeals</h1>
+        <button>Cart</button>
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="Table full of food" />
+      </div>
+    </>
+  );
+};
 
 export default Header;
